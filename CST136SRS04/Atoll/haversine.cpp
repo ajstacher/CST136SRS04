87a84332b9d75dee99d45e30cced34b328e66fcc
ajstacher <andrew.stacher@oit.edu>
2018-06-08 20:07:40 -0700

    Final Commit

--- CST136SRS04/Atoll/haversine.cpp
+++ CST136SRS04/Atoll/haversine.cpp
@@ -1,11 +1,7 @@
 #include "stdafx.h"
-#include <math.h>
 #include "haversine.h"
-#include "latitude.h"
-#include "longitude.h"
 #include "location.h"
 #include "angle.h"
-
 
 double haversine(const GPS::Location & location1, const GPS::Location & location2)
 {
@@ -17,10 +13,10 @@ double haversine(const GPS::Location & location1, const GPS::Location & location
 	//long2 * (myPi / 180.0)
 
 	//convert to signed decimal degrees, then convert to radians
-	const auto lat1r = (location1.getLatDeg() + (location1.getLatMin() / 60) + (location1.getLatSec() / 3600)) * (myPi / 180.0);
-	const auto lat2r = (location2.getLatDeg() + (location2.getLatMin() / 60) + (location2.getLatSec() / 3600)) * (myPi / 180.0);
-	const auto long1r = (location1.getLongDeg() + (location1.getLongMin() / 60) + (location1.getLongSec() / 3600)) * (myPi / 180.0);
-	const auto long2r = (location2.getLongDeg() + (location2.getLongMin() / 60) + (location2.getLongSec() / 3600)) * (myPi / 180.0);
+	const auto lat1r = (location1.getLatDeg() + (location1.getLatMin() / 60.0) + (location1.getLatSec() / 3600.0)) * (myPi / 180.0);
+	const auto lat2r = (location2.getLatDeg() + (location2.getLatMin() / 60.0) + (location2.getLatSec() / 3600.0)) * (myPi / 180.0);
+	const auto long1r = (location1.getLongDeg() + (location1.getLongMin() / 60.0) + (location1.getLongSec() / 3600.0)) * (myPi / 180.0);
+	const auto long2r = (location2.getLongDeg() + (location2.getLongMin() / 60.0) + (location2.getLongSec() / 3600.0)) * (myPi / 180.0);
 
 	distance = 2.0 * radius * asin(sqrt(pow(sin((lat2r - lat1r) / 2.0), 2) + cos(lat1r) * cos(lat2r) *  pow(sin((long2r - long1r) / 2.0), 2)));
 
--- CST136SRS04/Atoll/range.h
+++ CST136SRS04/Atoll/range.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <assert.h>
-
 template<typename T, T min, T max>
 class Range
 {
